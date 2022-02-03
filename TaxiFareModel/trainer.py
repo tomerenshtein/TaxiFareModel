b78fcd00ac481b813c7a08b245f55f6a854fe85c
Tomer Erenshtein <tomerenshtein@gmail.com>
2022-02-03 19:49:37 +0100

    added mlflow tracking

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -9,7 +9,15 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 
+from memoized_property import memoized_property
+
+import mlflow
+from mlflow.tracking import MlflowClient
+
+
 class Trainer():
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    EXPERIMENT_NAME = "[FR][PAR][tomerenshtein] TaxiFare 01"
 
 
     def __init__(self, X, y):
@@ -38,19 +46,48 @@ class Trainer():
                         ('linear_model', LinearRegression())])
         return self
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.EXPERIMENT_NAME).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
     def run(self):
         """set and train the pipeline"""
         self.set_pipeline()
         self.pipeline.fit(self.X, self.y)
+
         return self
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric("rmse", rmse)
+        self.mlflow_log_param("model", self.pipeline[1])
+
         print(rmse)
         return rmse
-
 
 
 
@@ -74,3 +111,7 @@ if __name__ == "__main__":
 
     # evaluate
     model.evaluate(X_test, y_test)
+
+    experiment_id = model.mlflow_experiment_id
+
+    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
